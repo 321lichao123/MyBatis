94be718a29b5b7c345fa2f4dad4eb5d94e923b5c
李超 <1246877006@qq.com>
2021-12-30 00:23:03 +0800

    mybatis的搭建

---
+++ myBatis_01_HelloWorld/src/com/atguigu/test/MyBatisTest.java
@@ -0,0 +1,26 @@
+package com.atguigu.test;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @ClassName MyBatisTest
+ * @Description
+ * @Author 12468
+ * @Date 2021/12/30 0:18
+ * @Version 1.0
+ */
+public class MyBatisTest {
+
+    @Test
+    public void test() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}
