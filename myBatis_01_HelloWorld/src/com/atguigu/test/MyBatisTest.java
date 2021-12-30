059f5021a8418951e2454a8a8ea716713d6c734c
lichao <1246877006@qq.com>
2021-12-30 18:07:08 +0800

    mybatis的使用

--- myBatis_01_HelloWorld/src/com/atguigu/test/MyBatisTest.java
+++ myBatis_01_HelloWorld/src/com/atguigu/test/MyBatisTest.java
@@ -1,6 +1,8 @@
 package com.atguigu.test;
 
+import com.atguigu.mybatis.Employee;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
@@ -17,10 +19,24 @@ import java.io.InputStream;
  */
 public class MyBatisTest {
 
+    /**
+     * 1、根据xml配置文件(全局配置文件)创建一个SqlSessionFactory对象
+     * @throws IOException
+     */
     @Test
     public void test() throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2、获取sqlSession实例能执行已经映射的sql语句
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Employee employee;
+        try {
+            employee = sqlSession.selectOne("com.atguigu.mybatis.EmployeeMapper.selectBlog", 1);
+            System.out.println("employee = " + employee);
+        } finally {
+            sqlSession.close();
+        }
     }
 }
